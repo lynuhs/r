53f628676f5519bbe6aab1f05f5310890411b84d
lynuhs <45751895+lynuhs@users.noreply.github.com>
2019-02-01 18:56:00 +0100

    Update Trustpilot.R

--- Trustpilot.R
+++ Trustpilot.R
@@ -17,6 +17,27 @@ factorToCharacter <- function(df){
   return(df)
 }
 
+lynuhs_theme <- function(){
+  bg <- "#b5f5ff"
+  lineCol <- "#7ec8d3"
+  
+  theme_bw() +
+    theme(plot.margin = unit(c(.5, .5, .5, .5),"cm")) +
+    theme(plot.background = element_rect(fill = bg)) + 
+    theme(panel.background = element_rect(fill = bg)) +
+    theme(panel.spacing = unit(3, "lines")) +
+    theme(panel.border = element_blank()) +
+    theme(panel.grid = element_line(colour = lineCol, linetype = "dotted")) +
+    theme(panel.grid.major.x = element_line(linetype = 0)) +
+    theme(strip.background = element_blank()) +
+    theme(strip.text = element_text(size = 14, color = "black", face = "bold")) +
+    theme(legend.key = element_blank()) +
+    theme(legend.title = element_blank()) + 
+    #theme(axis.text.x = element_text(angle = 30)) +
+    theme(plot.title = element_text(margin = unit(c(0.1,0.1,0.1,0.1),"cm"))) +
+    theme(plot.subtitle = element_text(margin = unit(c(0,0,1,0),"cm")))
+}
+
 trustpilot <- function(domain){
   url <- paste0("https://www.trustpilot.com/review/", domain, "?languages=all")
   
@@ -130,3 +151,10 @@ trustpilot <- function(domain){
 }
 
 hak <- trustpilot("huntakiller.com")
+
+ggplot(subset(hak, rating > 0), aes(x=as.factor(rating), fill=verifiedOrder)) + 
+  geom_bar(stat="count") + 
+  xlab("Rating") +
+  labs(fill = "Verified Order") + 
+  scale_fill_manual(values = c("#051d30","#3a6587")) +
+  lynuhs_theme()
