c67021b7dd7f0ed6582ee070a5a7b0b065e08905
lynuhs <45751895+lynuhs@users.noreply.github.com>
2020-09-08 19:40:29 +0200

    Update spotifyToSlackStatus.R

--- spotifyToSlackStatus.R
+++ spotifyToSlackStatus.R
@@ -9,76 +9,96 @@ user_id <- [ENTER_SLACK_MEMBER_ID]
 
 access_token <- get_spotify_access_token()
 
-current_track <- get_my_currently_playing()
 
+tryCatch({
+  current_track <- get_my_currently_playing()
 
-
-if(current_track$is_playing){
-  if(current_track$currently_playing_type == "episode"){
-    POST(
-      url = "https://slack.com/api/users.profile.set",
-      encode = "json",
-      add_headers(
-        .headers = c(
-          "Authorization" = bearer,
-          "Content-Type" = "application/json; charset=utf-8"
-        )
-      ),
-      body = list(
-        profile = list(
-          status_emoji = ":headphones:",
-          status_text = "Podcast"
-        ),
-        user = user_id
-      )
-    )
-  } else {
-    track <- current_track$item$name
-    artist <- paste(current_track$item$artists$name, collapse = ", ")
-    external_link <- current_track$item$external_urls$spotify
-    text = paste0(track, " (", artist, ")")
-    
-    POST(
-      url = "https://slack.com/api/users.profile.set",
-      encode = "json",
-      add_headers(
-        .headers = c(
-          "Authorization" = bearer,
-          "Content-Type" = "application/json; charset=utf-8"
-        )
-      ),
-      body = list(
-        profile = list(
-          status_emoji = ":headphones:",
-          status_text = text,
-          fields = list(
-            "Xf01A6HS6N0M" = list(
-              value = external_link,
-              alt = text
-            )
+  if(current_track$is_playing){
+    if(current_track$currently_playing_type == "episode"){
+      POST(
+        url = "https://slack.com/api/users.profile.set",
+        encode = "json",
+        add_headers(
+          .headers = c(
+            "Authorization" = bearer,
+            "Content-Type" = "application/json; charset=utf-8"
           )
+        ),
+        body = list(
+          profile = list(
+            status_emoji = ":headphones:",
+            status_text = "Podcast"
+          ),
+          user = user_id
+        )
+      )
+    } else {
+      track <- current_track$item$name
+      artist <- paste(current_track$item$artists$name, collapse = ", ")
+      external_link <- current_track$item$external_urls$spotify
+      text = paste0(track, " (", artist, ")")
+
+      POST(
+        url = "https://slack.com/api/users.profile.set",
+        encode = "json",
+        add_headers(
+          .headers = c(
+            "Authorization" = bearer,
+            "Content-Type" = "application/json; charset=utf-8"
+          )
+        ),
+        body = list(
+          profile = list(
+            status_emoji = ":headphones:",
+            status_text = text,
+            fields = list(
+             [CUSTOM_FIELD_ID] = list(
+                value = external_link,
+                alt = text
+              )
+            )
+          ),
+          user = user_id
+        )
+      )
+    }
+
+  } else {
+    POST(
+      url = "https://slack.com/api/users.profile.set",
+      encode = "json",
+      add_headers(
+        .headers = c(
+          "Authorization" = bearer,
+          "Content-Type" = "application/json; charset=utf-8"
+        )
+      ),
+      body = list(
+        profile = list(
+          status_emoji = "",
+          status_text = ""
         ),
         user = user_id
       )
     )
   }
-  
-} else {
+}, error = function(e){
   POST(
-    url = "https://slack.com/api/users.profile.set",
-    encode = "json",
-    add_headers(
-      .headers = c(
-        "Authorization" = bearer,
-        "Content-Type" = "application/json; charset=utf-8"
-      )
-    ),
-    body = list(
-      profile = list(
-        status_emoji = "",
-        status_text = ""
+      url = "https://slack.com/api/users.profile.set",
+      encode = "json",
+      add_headers(
+        .headers = c(
+          "Authorization" = bearer,
+          "Content-Type" = "application/json; charset=utf-8"
+        )
       ),
-      user = user_id
+      body = list(
+        profile = list(
+          status_emoji = "",
+          status_text = ""
+        ),
+        user = user_id
+      )
     )
-  )
-}
+  }
+})
