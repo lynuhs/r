6b783560ecb2c7f04b4d614aab7382d4a1ce6b42
lynuhs <45751895+lynuhs@users.noreply.github.com>
2020-09-08 19:16:08 +0200

    Create spotifyToSlackStatus.R

---
+++ spotifyToSlackStatus.R
@@ -0,0 +1,84 @@
+#devtools::install_github("lynuhs/spotifyr")
+library(spotifyr)
+library(httr)
+
+bearer <- "Bearer [ENTER_SLACK_OAUTH_TOKEN]"
+Sys.setenv(SPOTIFY_CLIENT_ID = [ENTER_SPOTIFY_API_CLIENT_ID])
+Sys.setenv(SPOTIFY_CLIENT_SECRET = [ENTER_SPOTIFY_API_CLIENT_SECRET])
+user_id <- [ENTER_SLACK_MEMBER_ID]
+
+access_token <- get_spotify_access_token()
+
+current_track <- get_my_currently_playing()
+
+
+
+if(current_track$is_playing){
+  if(current_track$currently_playing_type == "episode"){
+    POST(
+      url = "https://slack.com/api/users.profile.set",
+      encode = "json",
+      add_headers(
+        .headers = c(
+          "Authorization" = bearer,
+          "Content-Type" = "application/json; charset=utf-8"
+        )
+      ),
+      body = list(
+        profile = list(
+          status_emoji = ":headphones:",
+          status_text = "Podcast"
+        ),
+        user = user_id
+      )
+    )
+  } else {
+    track <- current_track$item$name
+    artist <- paste(current_track$item$artists$name, collapse = ", ")
+    external_link <- current_track$item$external_urls$spotify
+    text = paste0(track, " (", artist, ")")
+    
+    POST(
+      url = "https://slack.com/api/users.profile.set",
+      encode = "json",
+      add_headers(
+        .headers = c(
+          "Authorization" = bearer,
+          "Content-Type" = "application/json; charset=utf-8"
+        )
+      ),
+      body = list(
+        profile = list(
+          status_emoji = ":headphones:",
+          status_text = text,
+          fields = list(
+            "Xf01A6HS6N0M" = list(
+              value = external_link,
+              alt = text
+            )
+          )
+        ),
+        user = user_id
+      )
+    )
+  }
+  
+} else {
+  POST(
+    url = "https://slack.com/api/users.profile.set",
+    encode = "json",
+    add_headers(
+      .headers = c(
+        "Authorization" = bearer,
+        "Content-Type" = "application/json; charset=utf-8"
+      )
+    ),
+    body = list(
+      profile = list(
+        status_emoji = "",
+        status_text = ""
+      ),
+      user = user_id
+    )
+  )
+}
