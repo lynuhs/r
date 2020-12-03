e07ec0cd8866e9668dabffd62df1e0f658d9e1b2
lynuhs <45751895+lynuhs@users.noreply.github.com>
2020-12-03 18:34:02 +0100

    Update partitionBy.R

--- partitionBy.R
+++ partitionBy.R
@@ -1,6 +1,12 @@
 partitionByOver <- function(x, calcColumn, partitionBy, newColumn, FUN){
+  originalColOrder <- colnames(x)
+  x$originalRowOrder <- 1:nrow(x)
+  rowNames <- rownames(x)
+  
   partitionBy <- deparse(substitute(partitionBy))
   partitionBy <- strsplit(gsub("c\\(|\\)| ", "", partitionBy), ",")[[1]]
+  
+  newColumn <- deparse(substitute(newColumn))
   
   if(!all(partitionBy %in% colnames(x))){
     stop("One or more of the columns specified in partitionBy does not exist in the data frame", call. = FALSE)
@@ -18,9 +24,12 @@ partitionByOver <- function(x, calcColumn, partitionBy, newColumn, FUN){
     as.data.frame()
   options(dplyr.summarise.inform = TRUE)
   
-  colnames(pdf) <- c(partitionBy, deparse(substitute(newColumn)))
+  colnames(pdf) <- c(partitionBy, newColumn)
+
   
   x <- merge(x, pdf, partitionBy, all.x=TRUE)
+  x <- x[order(x$originalRowOrder), c(originalColOrder, newColumn)]
+  rownames(x) <- rowNames
   
   return(x)
 }
